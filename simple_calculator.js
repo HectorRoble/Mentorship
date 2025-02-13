4d8369b88b9cd487fe6c20c1c3a4ce24ef51a272
Marcio Hector Robles <134791398+HectorRoble@users.noreply.github.com>
2025-02-13 17:48:27 -0300

    Update simple_calculator.js

---
+++ simple_calculator.js
@@ -0,0 +1,8 @@
+function rainAmount(mm){
+  switch (true){
+    case (mm<0);
+      retun "You need to give your plant"+(40 - mm)+ "mm of water";
+    default:
+      return "You plant has had more than enogh water for today!";
+  }
+}
